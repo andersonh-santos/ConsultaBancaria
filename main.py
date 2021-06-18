be480b6799386070223ca6ea18eec78f077e1dfc
Anderson Santos <42190719+andersonh-santos@users.noreply.github.com>
2021-06-17 23:59:09 -0300

    Dicionário da tabela de contas

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-import xlrd, os
-from zipfile import Path
-
+import xlrd
+import os
 
 PATH = os.path.abspath("data/sheet_money.xlsx")
 WORKBOOK = xlrd.open_workbook(PATH)
 RECORDS_WORKSHEET = WORKBOOK.sheet_by_index(0)
 ACCOUNTS_WORKSHEET = WORKBOOK.sheet_by_index(1)
 
-def account_records():
-    for row in range(ACCOUNTS_WORKSHEET.nrows):
-        for col in range(ACCOUNTS_WORKSHEET.ncols):
-                print(ACCOUNTS_WORKSHEET.cell_value(row, col), end = '')
-                print('\t', end = '')
-        print()
 
-account_records()
+def accounts_client_acronym():
+    account_names = list()
+    clients = list()
+    acronyms = list()
+    for row in range(1, ACCOUNTS_WORKSHEET.nrows):
+        evaluated_row = ACCOUNTS_WORKSHEET.row(row)
+        account_name = evaluated_row[0].value
+        client = evaluated_row[1].value
+        acronym = evaluated_row[2].value
 
-def register_records():
-    for row in range(RECORDS_WORKSHEET.nrows):
-        for col in range(RECORDS_WORKSHEET.ncols):
-                print(RECORDS_WORKSHEET.cell_value(row, col), end = '')
-                print('\t', end = '')
-        print()
+        account_names.append(account_name)
+        clients.append(client)
+        acronyms.append(acronym)
 
-register_records()
+
+    # print(f'Nomes: {account_names}')
+    # print(f'Clientes: {clients}')
+    # print(f'Siglas: {acronyms}')
+
+
+    client_object_list = list()
+
+    for i in range(len(account_names)):
+        client_object_list.append(
+            {
+                'account_name': account_names[i],
+                'client': clients[i],
+                'acronym': acronyms[i],
+            }
+        )
+
+    print(client_object_list)
+
+accounts_client_acronym()
